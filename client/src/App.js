60556f773012b8ac9fb00d075fd675ab8ba0ca4e
Michael Williams <manicdesigner@gmail.com>
2019-10-12 12:52:15 -0400

    pulling data from api for display on screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navbar from './Navbar';
+import SoccerData from './api/SoccerData';
 import './App.scss';
 
-function App() {
-  return (
-    <div className='App'>
-      <Navbar />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      players: []
+    };
+  }
+
+  setPlayers = data => {
+    this.setState({
+      players: data
+    });
+  };
+
+  render() {
+    console.log('p', this.state.players);
+    return (
+      <div className='App'>
+        <SoccerData setPlayers={this.setPlayers} />
+        <Navbar />
+      </div>
+    );
+  }
 }
 
 export default App;
