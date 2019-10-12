d34cc8f8ff9180d28659b49a21f4852ad711e859
Michael Williams <manicdesigner@gmail.com>
2019-10-12 14:35:01 -0400

    test id

--- client/src/player/Player.js
+++ client/src/player/Player.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Player(props) {
   return (
-    <div className='player'>
+    <div className='player' data-testid='player-data'>
       <h2>{props.name}</h2>
       <p>{props.country}</p>
       <p>Search Result: {props.searches}</p>
