78519b98d2f9c7e82b845bcf2008395d43cb69aa
Michael Williams <manicdesigner@gmail.com>
2019-10-12 12:37:27 -0400

    soccer player data api catch

---
+++ client/src/api/SoccerData.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class SoccerData extends Component {
+  constructor() {
+    super();
+    this.state = {
+      players: []
+    };
+  }
+}
